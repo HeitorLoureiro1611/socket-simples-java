43cc6a43a8ec61824331d5ec6a6b0662ec9cc22e
HeitorLoureiro1611 <heitor.loureiro1611@gmail.com>
2026-09-14 17:44:54 -0300

    Sistema de log das menságens

--- cliente.java
+++ cliente.java
@@ -3,19 +3,23 @@ import java.io.PrintStream;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class cliente
-{
-    public static void main() throws IOException {
-        Socket client = new Socket("127.0.0.1", 3030);
-        PrintStream envio = new PrintStream(client.getOutputStream());
-        Scanner mensagem = new Scanner(System.in);
-        while(true)
+public class cliente {
+    public static void main(String[] args) throws IOException {
         {
-            envio.println(mensagem.nextLine());
+            // inicialização do cliente
+            Socket client = new Socket("127.0.0.1", 4000);
+            //stream representa o sistema de menságeria
+            PrintStream envio = new PrintStream(client.getOutputStream(), true); // autoflush ativo com o true
+            // scanner pra receber o input do usuário
+            Scanner mensagem = new Scanner(System.in);
+
+            System.out.println("Mensageria aberta, envie as menságens para o servidor");
+            System.out.println("Para fechar a conexão basta fechar o programa (CTRL + C)");
+
+            // sistema incrível do envio das menságens
+            while (true) {
+                envio.println(mensagem.nextLine());
+            }
         }
-
-
-        //envio.close();
-        //client.close();
     }
 }
--- server.java
+++ server.java
@@ -2,32 +2,43 @@ import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
-public class server
-{
-     static void main() throws IOException{
-         Socket cliente;
-        ServerSocket server = new ServerSocket(3030);
-        if(!server.isBound()){
-            server.bind(new InetSocketAddress("192.168.0.1", 0));
+public class server {
+    public static void main(String[] args) throws IOException {
+        {
+            // cria o socket e inicializa ele na porta 4000
+            Socket cliente;
+            ServerSocket server = new ServerSocket(4000);
+
+            // cria a pasta onde vai organizar os arquivos
+            File logs = new File("log");
+            if (!logs.exists())
+                logs.mkdir();
+
+            // cria o arquivo
+            FileWriter escrevedor = new FileWriter(new File(logs, "tarefas.txt"), true);
+
+            // só pra mostrar algumas informações
+            System.out.println("Servidor iniciado em " + server.getInetAddress().getHostAddress() + ":" + server.getLocalPort());
+            InetAddress inet = server.getInetAddress();
+            System.out.println("HostAddress=" + inet.getHostAddress());
+            System.out.println("HostName=" + inet.getHostName());
+            System.out.println("Porta = " + server.getLocalPort());
+            System.out.println("Está aberto?: " + server.isBound());
+
+            // função assincrona que espera a conexão do cliente
+            cliente = server.accept();
+            Scanner mensagem = new Scanner(cliente.getInputStream());
+            System.out.println("\n\nCliente: " + cliente.getInetAddress());
+            System.out.println("\n");
+
+            while (mensagem.hasNextLine()) {
+                String linha = mensagem.nextLine();
+                System.out.println(cliente.getLocalAddress() + " - " + linha);
+                escrevedor.append(String.valueOf(cliente.getLocalAddress())).append(linha).append(System.lineSeparator());
+                // System.lineSeparator() desconcatena as mensagens
+                escrevedor.flush(); // fazer flush no buffer
+            }
+            server.close();
         }
-
-         System.out.println("Servidor iniciado em " + server.getInetAddress().getHostAddress() + ":" + server.getLocalPort());
-         InetAddress inet = server.getInetAddress();
-         System.out.println("HostAddress="+inet.getHostAddress());
-         System.out.println("HostName="+inet.getHostName());
-         System.out.println("Porta = "+server.getLocalPort());
-         System.out.println("Está aberto?: "+ server.isBound());
-
-         cliente = server.accept();
-         Scanner mensagem = new Scanner(cliente.getInputStream());
-
-         System.out.println("Cliente: " + cliente.getInetAddress());
-        System.out.println("\n");
-
-        while(mensagem.hasNextLine()){
-            System.out.println(cliente.getLocalAddress() + " - " + mensagem.nextLine());
-        }
-
-        server.close();
-     }
+    }
 }
